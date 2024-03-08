7ae963aed0a42b603246b12249ef72f298590623
unknown <motta.phenrique@gmail.com>
2024-03-07 22:09:21 -0300

    vai

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 let index = 0;
 const imagens = document.querySelectorAll('.carousel img');
 
+
 function mostrarImagem() {
     imagens.forEach(img => img.classList.remove('active'));
     imagens[index].classList.add('active');
     index = (index + 1) % imagens.length;
 }
 
-setInterval(mostrarImagem, 4000); // Troca de imagem a cada 3 segundos (3000 milissegundos)
+
+setInterval(mostrarImagem, 3000); // Troca de imagem a cada 3 segundos (3000 milissegundos)
